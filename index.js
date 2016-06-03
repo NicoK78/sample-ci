d417a342218d7810d2a0cbabc8b3d537bf96680c
NicoK78 <nicolas.kervoern@gmail.com>
2016-06-03 16:25:22 +0200

    add features

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ var api = express();
 var DEFAULT_PORT = 3000;
 var contacts = [];
 
-api.get('/', function(req, res, next) {
-	res.send('Hello world');
-});
+api.use(express.static('public'));
 
-api.get('/contacts', function(req, res, next) {
+api.get('/api/contacts', function(req, res, next) {
 	res.send(contacts);
 });
 
-api.get('/contacts/:name', function(req, res, next) {
+api.get('/api/contacts/:name', function(req, res, next) {
 	res.send(contacts);
 });
 
-api.post('/contacts', bodyparser.json(), function(req, res, next) {
+api.post('/api/contacts', bodyparser.json(), function(req, res, next) {
 	var contact = req.body.contact;
 	if(typeof contact !== 'object')
 		return res.status(422).send('Unprocessable entity');
@@ -26,7 +24,7 @@ api.post('/contacts', bodyparser.json(), function(req, res, next) {
 	res.send(contact);
 });
 
-api.put('/contacts/:name/:new', function(req, res, next) {
+api.put('/api/contacts/:name/:new', function(req, res, next) {
 	var count = 0;
 	contacts = contacts.map(function(contact) {
 		if(contact.name === req.params.name) {
@@ -39,7 +37,7 @@ api.put('/contacts/:name/:new', function(req, res, next) {
 	res.send({count: count});
 });
 
-api.delete('/contacts/:name', function(req, res, next) {
+api.delete('/api/contacts/:name', function(req, res, next) {
 	var count = 0;
 	contacts = _.remove(contacts, function(contact) {
 		if (contact.name !== req.params.name) {
