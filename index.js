00ee017889b646d4088bb76a81d1419cf1615a6c
NicoK78 <nicolas.kervoern@gmail.com>
2016-06-03 15:03:03 +0200

    added GET /contacts

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ api.get('/', function(req, res, next) {
 	res.send('Hello world');
 });
 
+api.get('/contacts', function(req, res, next) {
+	res.send([]);
+})
+
 console.log('API listening on port 3000');
 api.listen(3000);
 
